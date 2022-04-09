b05a67c1d8d434dc170bc84ef902ed19fc45950c
Jessie Guzman <jessieguzman777@gmail.com>
2022-04-09 14:13:44 -0500

    added dropbox

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,16 +1,36 @@
 document.addEventListener('DOMContentLoaded', ()=>{
 getApiInfo()
-renderAllAnime()
 })
 
 const getApiInfo = () => {
   fetch('https://anime-facts-rest-api.herokuapp.com/api/v1')
   .then(resp => resp.json())
-  .then((anime) => anime.forEach(titles => renderAllAnime(titles)))
+  .then((data) => data.data.forEach(titles => dropDownButton(titles)))
+  //anime.forEach(titles => renderAllAnime(titles)))
 }
 
-const renderAllAnime = (titles) => {
-  let anime = titles
-  console.log(anime)
+// const renderAllAnime = (titles) => {
+//   const animeName = document.createElement('h2')
+//   animeName.innerText = titles.anime_name
 
+// const animeselection = document.querySelector('h3')
+// animeselection.append(animeName)
+// }
+
+function dropDownButton() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    let i;
+    for (i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
 }
